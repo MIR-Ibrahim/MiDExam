c0edbc763354c8b228d6aac94aa8de222585f8dc
MIR-Ibrahim <mohammadibrahim.rajput@gmail.com>
2023-05-27 17:27:25 +0500

    Add Timer Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import PoolDisplay from './PollDisplay';
+import UserParticipation from './UserParticipation';
+import AnotherUserParticipation from './AddAnotheUser';
+import ThankYou from './ThankYouMessage';
 
 function App() {
   return (
     <div className="App">
-      <h1>Sukkur IBA University</h1>
+      <h1>Pool Display</h1>
+      <PoolDisplay />
+      <UserParticipation/>
+      <AnotherUserParticipation/>
+      <ThankYou/>
     </div>
   )
 }
